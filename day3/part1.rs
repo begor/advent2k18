57b97de6b99dde5572da1fdb2dfeca9964a26d75
Egor Biriukov <b-egor@yandex-team.ru>
2018-12-25 11:36:02 +0300

    Day 3: parse claims

---
+++ day3/part1.rs
@@ -0,0 +1,56 @@
+use std::io::{self, BufRead};
+
+
+#[derive(Debug)]
+struct Claim {
+    left_edge: i32,
+    top_edge: i32,
+    wide: i32,
+    tall: i32
+}
+
+
+fn read_claims_strings() -> Vec<String> {
+    let stdin = io::stdin();
+
+
+    return stdin.lock().lines().map(|s| s.unwrap()).collect();
+}
+
+
+fn parse_coordinates(coordinates: &str) -> (i32, i32) {
+    let v: Vec<&str> = coordinates.split(|c: char| c == ':' || c == ',').collect();
+
+    return (v[0].parse::<i32>().unwrap(), v[1].parse::<i32>().unwrap());
+}
+
+
+fn parse_dimensions(dimensions: &str) -> (i32, i32) {
+    let v: Vec<&str> = dimensions.split("x").collect();
+
+    return (v[0].parse::<i32>().unwrap(), v[1].parse::<i32>().unwrap());
+}
+
+
+fn parse_claims_string(claims_string: &String) -> Claim {
+    let v: Vec<&str> = claims_string.split(' ').collect();
+    let (_, _, coordinates, dimensions) = (v[0], v[1], v[2], v[3]);
+    
+    let (left_edge, top_edge) = parse_coordinates(coordinates);
+    let (wide, tall) = parse_dimensions(dimensions);
+
+    return Claim { left_edge: left_edge, top_edge: top_edge, wide: wide, tall: tall }
+}
+
+
+fn find_overlaps_over_claims(claims: Vec<Claim>) {
+    println!("{:?}", claims);
+
+}
+
+
+fn main() {
+    let claims_strings = read_claims_strings();
+    let claims = claims_strings.iter().map(parse_claims_string).collect::<Vec<_>>();
+    find_overlaps_over_claims(claims);
+}
